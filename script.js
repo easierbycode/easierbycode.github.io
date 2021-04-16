11a353a2567147409cd5b990b945b2007a936d6c
easierbycode <daniel@easierbycode.com>
2021-04-15 17:12:15 -0700

    add iframe loader

---
+++ script.js
@@ -0,0 +1,23 @@
+$('.trig').click(function (evt) {
+  evt.stopPropagation();
+
+  $('iframe, footer').css('opacity', 0);
+  setTimeout(function () {
+    $('iframe').remove();
+  }, 1300);
+});
+
+$('.lab').click(function () {
+  let ID = $(this).data('iframe');
+
+  setTimeout(function () {
+    $('body').append(`<iframe height="100%" style="width: 100%;" scrolling="no" title="${ID}" src="http://codemonkey.games/${ID}" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true"></iframe>`);
+  }, 1500);
+
+  setTimeout(function () {
+    $('iframe, footer').css('opacity', 1);
+  }, 1600);
+
+});
+
+
