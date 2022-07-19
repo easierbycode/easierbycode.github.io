f7f47230a44cd089a9839c5539d9c53d7689bbd6
▓▒░ ♔ Daniel ♔ ░▒▓ <563975+easierbycode@users.noreply.github.com>
2022-07-18 18:03:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $('.lab').click(function () {
   let ID = $(this).data('iframe');
 
   setTimeout(function () {
-    $('body').append(`<iframe height="100%" style="width: 100%;" scrolling="no" title="${ID}" src="http://codemonkey.games/${ID}" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true"></iframe>`);
+    $('body').append(`<iframe height="100%" style="width: 100%;" scrolling="no" title="${ID}" src="https://easierbycode.github.io/${ID}" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true"></iframe>`);
   }, 1500);
 
   setTimeout(function () {
